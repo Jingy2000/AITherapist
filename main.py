f31683a2b2c1606026ab5999f5a10a443c63d960
Junyao-SG <junyao0702@gmail.com>
2024-04-11 19:57:12 -0400

    re-layout sidebar & minor updates

--- main.py
+++ main.py
@@ -28,21 +28,6 @@ def send_post_request(local_model_name: str):
 with st.sidebar:
     st.markdown("<h1 style='text-align:left;font-family:Georgia'>AI Therapist </h1>",
                 unsafe_allow_html=True)
-    st.markdown("An AI-powered therapy application designed to be your virtual companion "
-                "on your journey toward better mental well-being. "
-                "Built with advanced natural language processing and machine learning algorithms, "
-                "AI Therapist provides a safe, non-judgmental space for you "
-                "to express your thoughts, concerns, and emotions.")
-    st.markdown("Our AI therapist is trained to actively listen, empathize, "
-                "and provide personalized insights and coping strategies tailored to your unique situation. "
-                "Whether you're dealing with anxiety, depression, relationship issues, "
-                "or simply seeking self-improvement, "
-                "AI Therapist is here to support you every step of the way.")
-    st.markdown("<h2 style='text-align:left;font-family:Georgia'>Features</h2>",
-                unsafe_allow_html=True)
-    st.markdown(" - one")
-    st.markdown(" - two")
-    st.divider()
 
     with st.form("config"):
         st.header("Configuration")
@@ -57,13 +42,14 @@ with st.sidebar:
             placeholder="only for gpt-3.5-turbo",
             type="password"
             )
-        temperature = st.slider("Temperature", 0.0, 10.0, 0.1, 0.1, format="%.1f")
-        if st.form_submit_button("Save"):
+        temperature = st.slider("Temperature", 0.0, 2.0, 0.0, 0.1, format="%.1f")
+        if st.form_submit_button("Submit"):
             ss.model_config = {
                 "openai_api_key": openai_api_key,
                 "model": model,
                 "temperature": temperature,
             }
+
             if ss.model_config['model'] in ["llama2", "mistral"]:
                 with st.spinner(text="Loading model ..."):
                     response = send_post_request(ss.model_config['model'])
@@ -71,18 +57,51 @@ with st.sidebar:
                         st.success('Model Loaded!')
                     else:
                         st.error('This is an error', icon="🚨")
-                    pass
+
+            if ss.model_config['model'] == "gpt-3.5-turbo":
+                # Get an OpenAI API Key before continuing
+                # Attempt to retrieve API key from secrets
+                try:
+                    openai_api_key = st.secrets["openai_api_key"]
+                except (FileNotFoundError, KeyError):
+                    openai_api_key = ss.model_config['openai_api_key']
+
+                if not openai_api_key:
+                    st.info("Enter an OpenAI API Key to continue")
+                    st.stop()
     
+                try:
+                    response = ChatOpenAI(temperature=0,
+                                          max_tokens=2,
+                                          api_key=openai_api_key).invoke("Hello")
+                except Exception as e:
+                    st.warning('Please enter your valid OpenAI API key!', icon='⚠')
+                    st.stop()
+
+    st.divider()
+    st.markdown("<h2 style='text-align:left;font-family:Georgia'>Introduction</h2>",
+                unsafe_allow_html=True)
+    st.markdown("An AI-powered therapy application designed to be your virtual companion "
+                "on your journey toward better mental well-being. "
+                "Built with advanced natural language processing and machine learning algorithms, "
+                "AI Therapist provides a safe, non-judgmental space for you "
+                "to express your thoughts, concerns, and emotions.")
+    st.markdown("Our AI therapist is trained to actively listen, empathize, "
+                "and provide personalized insights and coping strategies tailored to your unique situation. "
+                "Whether you're dealing with anxiety, depression, relationship issues, "
+                "or simply seeking self-improvement, "
+                "AI Therapist is here to support you every step of the way.")
+    st.markdown("<h2 style='text-align:left;font-family:Georgia'>Features</h2>",
+                unsafe_allow_html=True)
+    st.markdown(" - one")
+    st.markdown(" - two")
 
 st.divider()
-st.markdown("<h4 style='text-align:left;font-family:Georgia'>"
-            "Please select a model and/or enter a valid key.</h4>",
-            unsafe_allow_html=True)
-
-
-
 
 if not "model_config" in ss:
+    st.markdown("<h4 style='text-align:left;font-family:Georgia'>"
+                "Please select a model and/or enter a valid key</h4>",
+                unsafe_allow_html=True)
     st.stop()
 
 msgs = StreamlitChatMessageHistory(key="chat_messages")
@@ -90,25 +109,7 @@ msgs = StreamlitChatMessageHistory(key="chat_messages")
 if len(msgs.messages) == 0:
     msgs.add_ai_message("Hi, how are you doing today!")
 
-view_messages = st.expander("View the message contents in session state")  
-
-if ss.model_config['model'] == "gpt-3.5-turbo":
-    # Get an OpenAI API Key before continuing
-    # Attempt to retrieve API key from secrets
-    try:
-        openai_api_key = st.secrets["openai_api_key"]
-    except (FileNotFoundError, KeyError):
-        openai_api_key = ss.model_config['openai_api_key']
-
-    if not openai_api_key:
-        st.info("Enter an OpenAI API Key to continue")
-        st.stop()
-    
-    try:
-        response = ChatOpenAI(temperature=0, api_key=openai_api_key).invoke("Hello")
-    except Exception as e:
-        st.warning('Please enter your valid OpenAI API key!', icon='⚠')
-        st.stop()
+# view_messages = st.expander("View the message contents in session state")
 
 # Set up the LangChain, passing in Message History
 prompt = ChatPromptTemplate.from_messages(
@@ -125,8 +126,7 @@ prompt = ChatPromptTemplate.from_messages(
 
 if ss.model_config['model'] == "gpt-3.5-turbo":
     chain = prompt | ChatOpenAI(temperature=ss.model_config['temperature'],
-                                api_key=openai_api_key
-                                )
+                                api_key=openai_api_key)
 elif ss.model_config['model'] in ["llama2", "mistral"]:
     if ss.model_config['model'] == "llama2":
         stop_tokens = ["[INST]", "[/INST]", "<<SYS>>", "<</SYS>>"]
@@ -135,8 +135,7 @@ elif ss.model_config['model'] in ["llama2", "mistral"]:
     chain = prompt | ChatOllama(model=ss.model_config['model'],
                                 base_url='http://ollama:11434',
                                 stop=stop_tokens,
-                                temperature=ss.model_config['temperature'],
-                                )
+                                temperature=ss.model_config['temperature'])
 
 chain_with_history = RunnableWithMessageHistory(
     chain,
@@ -158,5 +157,5 @@ if prompt := st.chat_input():
     st.chat_message("ai").write_stream(response)
 
 # Draw the messages at the end, so newly generated ones show up immediately
-with view_messages:
-    view_messages.json(st.session_state.chat_messages)
+# with view_messages:
+#     view_messages.json(st.session_state.chat_messages)
